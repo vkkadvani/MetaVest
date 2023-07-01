e635c470ced2fc2f7b525473ffb19a5163d6941b
Vivek kadvani <vkkadvani8866@gmail.com>
2023-07-01 14:59:45 +0530

    done

--- serverSide/src/middleware/loginMiddleware.js
+++ serverSide/src/middleware/loginMiddleware.js
@@ -1,28 +1,22 @@
-const express = require('express')
-const app = express()
-const jwt = require('jsonwebtoken')
-
-
+const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    try {
-        const { accsessToken, secretkey } = req.body
-        jwt.verify(accsessToken, secretkey, (err, decode) => {
-            if (err) {
-                console.log("middleware error : ", err);
-                res.status(400).json({ verify: false })
-            }
-            else {
-                next()
-            }
-        })
-    }
-    catch (e) {
-        console.log("middleware error", e)
-        res.status(400).json({ error: e })
-    }
-}
+  try {
+    const { accsessToken, secretkey } = req.body;
+    jwt.verify(accsessToken, secretkey, (err, decode) => {
+      if (err) {
+        console.log('middleware error : ', err);
+        res.status(400).json({ verify: false });
+      } else {
+        next();
+      }
+    });
+  } catch (e) {
+    console.log('middleware error', e);
+    res.status(400).json({ error: e });
+  }
+};
 
 module.exports = {
-    verifyToken
-}
+  verifyToken,
+};
